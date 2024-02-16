cd57a7213848ab864e7d7ea1c10e0107541449c9
JasonEvan <7ason3van@gmail.com>
2024-02-16 21:41:57 +0700

    Menambah javascript dan mengubah onclick fungsi

---
+++ script.js
@@ -0,0 +1,14 @@
+const wa = document.getElementById('wa');
+wa.addEventListener('click', () => {
+    window.location.href = 'https://wa.me/+62895413315500';
+});
+
+const ig = document.getElementById('ig');
+ig.addEventListener('click', () => {
+    window.location.href = 'https://instagram.com/jasonevansaputra';
+});
+
+const github = document.getElementById('github');
+github.addEventListener('click', () => {
+    window.location.href = 'https://github.com/JasonEvan';
+});
